e4e2bb9b415404c419368b523d8b50b576f3aedc
Andrew Haberman <andrew@websbydrew.com>
2014-01-28 10:05:54 -0500

    update

--- Python Files/Exercise Files/Ch5/htmlparsing_start.py
+++ Python Files/Exercise Files/Ch5/htmlparsing_start.py
@@ -6,6 +6,12 @@
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
+
+  # open the sample HTML file and read it
+  f = open("samplehtml.html")
+  if f.mode == "r":
+    contents = f.read() # read the entire file
+    parser.feed(contents)
     
 
 if __name__ == "__main__":
--- Python Files/Exercise Files/Ch5/inetdata_start.py
+++ Python Files/Exercise Files/Ch5/inetdata_start.py
@@ -2,5 +2,18 @@
 # Example file for retrieving data from the internet
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+import urllib2
+
+def main():
+  # open a connection to a URL using urllib2
+  webUrl = urllib2.urlopen("http://websbydrew.com")
+
+  # get the result code and print it
+  print "result code: " + str(webUrl.getcode())
+
+  # read the data from the URL and print it
+  data = webUrl.read()
+  print data
+
 if __name__ == "__main__":
   main()
